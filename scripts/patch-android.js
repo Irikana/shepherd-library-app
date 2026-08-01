853628d0d5b97d38b403231d93fb5ec07013ed25
QiYan Hua <253306658+Irikana@users.noreply.github.com>
2026-08-02 00:31:24 +0800

    fix: add SDK versions to dom-webview after removing gradle plugin
    The expo-module-gradle-plugin provided compileSdkVersion/buildToolsVersion
    via useDefaultAndroidSdkVersions(). After removing the plugin reference,
    these must be set explicitly from rootProject.ext.

--- scripts/patch-android.js
+++ scripts/patch-android.js
@@ -1,6 +1,7 @@
 /**
  * Postinstall patch: fix Expo SDK 52 Android build issues
- * 1. Remove expo-module-gradle-plugin reference from @expo/dom-webview
+ * 1. Replace @expo/dom-webview build.gradle: remove expo-module-gradle-plugin
+ *    and add explicit SDK version config (plugin provided these via useDefaultAndroidSdkVersions)
  * 2. Guard components.release in ExpoModulesCorePlugin.gradle
  */
 const fs = require('fs');
@@ -24,22 +25,50 @@ function patchFile(filePath, search, replace) {
 
 console.log('[patch-android] Applying Android build fixes...');
 
-// 1. Fix @expo/dom-webview: remove expo-module-gradle-plugin
+// 1. Fix @expo/dom-webview: replace entire plugins+android block
 const domWebviewGradle = path.join(__dirname, '..', 'node_modules', '@expo', 'dom-webview', 'android', 'build.gradle');
 patchFile(
   domWebviewGradle,
   `plugins {
   id 'com.android.library'
   id 'expo-module-gradle-plugin'
+}
+
+group = 'expo.modules.webview'
+version = '57.0.1'
+
+android {
+  namespace "expo.modules.webview"
+  defaultConfig {
+    versionCode 1
+    versionName "57.0.1"
+  }
 }`,
   `plugins {
   id 'com.android.library'
+}
+
+group = 'expo.modules.webview'
+version = '57.0.1'
+
+android {
+  namespace "expo.modules.webview"
+
+  // SDK versions (normally provided by expo-module-gradle-plugin via useDefaultAndroidSdkVersions)
+  compileSdkVersion rootProject.ext.compileSdkVersion
+  buildToolsVersion rootProject.ext.buildToolsVersion
+
+  defaultConfig {
+    minSdkVersion rootProject.ext.minSdkVersion
+    targetSdkVersion rootProject.ext.targetSdkVersion
+    versionCode 1
+    versionName "57.0.1"
+  }
 }`
 );
 
 // 2. Fix ExpoModulesCorePlugin: guard components.release
 const expoPluginGradle = path.join(__dirname, '..', 'node_modules', 'expo-modules-core', 'android', 'ExpoModulesCorePlugin.gradle');
-const pluginContent = fs.readFileSync(expoPluginGradle, 'utf8');
 const releasePattern = 'release(MavenPublication) {\n          from components.release\n        }';
 const releaseReplacement = "if (components.findByName('release') != null) {\n          release(MavenPublication) {\n            from components.release\n          }\n        }";
 patchFile(expoPluginGradle, releasePattern, releaseReplacement);
